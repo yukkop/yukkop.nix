daa597871008c1aa9ac428b2bcdf6aa7ad38a6c1
ISNUFFI <60103896+ISNUFFI@users.noreply.github.com>
2024-07-09 16:42:19 +0300

    Update ariadne.nix
    added snuff ssh key

--- module/system/ariadne.nix
+++ module/system/ariadne.nix
@@ -26,7 +26,8 @@ in
   zramSwap.enable = true;
   networking.domain = "";
   users.users.root.openssh.authorizedKeys.keys = [
-    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAK+DoSzaE5ic5ccTppjJjsVrMQj7oLyW+4S8fB2dUgG yukkop@home'' 
+    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAK+DoSzaE5ic5ccTppjJjsVrMQj7oLyW+4S8fB2dUgG yukkop@home''
+    ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCudwg8NGJElcgwsiQcnxWkLpN8VvsEZo7Wf9e7MJNeDL1dMU5kLM/KdIa9FLW6ljzgw/uaah7ZpidYn0t0zUqiZSlytB/thAlke/IyWr3EbvOWUN8MtcAYnqRWDMMxNR5VzYgKDdxJmvEOuEDjTAtpSWRGJ87tPwZezORNrILYJLR6/pLmqL6NMfPcRPBM3DIUNXJS03Wx1b94NoMYh/QK8NUIW8H1fCRdiQBWTkgCbl5urnktPlFS4BPDl7IuNUbILy49IAS7OquCRIK2EyccXFEs6xk/IP+YvdTiZubi6B6zkj02bABAaWHMQdtKm4+9bAOWX2Pc80wI8ZcxL1XQoVVMUdDL2qg6JETqL0jPU1wKXNmjBl+Xc2WjfO6V9s0/H23QR8dtskON6JwZ41x6QaOxfRm/NNc3gqz+nWvyE2uEDLhzd6Y9OCu4d4F+9vzFIYiVfc2irfkA0a/l+86U/IBdD4GGmAvVwZmRNHbDmzhsmIkgZfHCVzT6Nvwf71c= snuff@jorge-desktop''
   ];
 
   /* ssh */
