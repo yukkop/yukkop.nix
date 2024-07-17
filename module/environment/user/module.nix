beba44dbe312e5b01af3972ad6632369c7062580
yukkop <hectic.yukkop@gmail.com>
2024-07-17 18:41:33 +0000

    feat!: still try make user module

--- module/environment/user/module.nix
+++ module/environment/user/module.nix
@@ -1,30 +1,26 @@
-{ outputs, lib, ... }:
+{ lib, ... }:
 let 
-  userOpts = { pkgs, name, config, ... }: {
-    imports = (outputs.lib.readSubModulesAsList ./.);
-
-    options = {};
-
-    config = with lib; mkMerge
-      [{ name = mkDefault name; }];
-  };
+  readUsers = path: 
+    with lib; 
+    with builtins; let
+      paths = pipe (readDir path) [
+        (filterAttrs (name: value: value == "regular" && hasSuffix ".nix" name && name != "module.nix"))
+	attrNames
+        (map (name: { 
+	  name = builtins.replaceStrings [".nix"] [""] name;
+	  value = { enable = mkEnableOption ""; };
+	}))
+	listToAttrs
+      ];
+    in
+      paths;
 in
 {
+  user = readUsers ./user;
 
-  options = with lib; {
-    preset.user = mkOption {
-      default = {};
-      type = with types; attrsOf (submodule userOpts);
-      example = {
-        yukkop.enable = true;
-      };
-      description = ''
-        precreated user modules
-      '';
-    };
+  options = {
   };
 
   config = {
-    preset.user.yukkop.enable = true;
   };
 }
